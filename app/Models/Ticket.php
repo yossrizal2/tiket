e59608d8e54ec56630f580d5b1a3fe73f5dc92a2
yossrizalr <yossrizalr@gmail.com>
2023-07-04 12:31:39 +0700

    update readme and migrate

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -14,6 +14,7 @@ class Ticket extends Model
     protected $fillable = [
         'id_card_number',
         'name',
-        'address'
+        'address',
+        'status'
     ];
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,14 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('123456')
         ));
 
+        \App\Models\Ticket::create(array(
+            'unique_id' => strtoupper(substr(md5(microtime()), 0, 8)),
+            'id_card_number' => '9982100160346',
+            'name' => 'King Khalid',
+            'address' => 'Saudi Arabia',
+            'status' => 0
+        ));
+
         // \App\Models\Portfolio::create(array(
         //     'user_id' => 11,
         //     'title' => 'Yossrizal Ramadhan',
